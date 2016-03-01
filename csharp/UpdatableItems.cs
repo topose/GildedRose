ca44d61d4a1f89c580a72f13feb9df45709b3248
topose <david.carballo.gm@gmail.com>
2016-03-01 18:41:28 +0100

    Added UpdatableItems list

---
+++ csharp/UpdatableItems.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GildedRose
+{
+    class UpdatableItems : IEnumerable<UpdatableItem>
+    {
+        private IEnumerable<Item> items;
+
+        public UpdatableItems(IEnumerable<Item> items)
+        {
+            this.items = items;
+        }
+
+        public IEnumerator<UpdatableItem> GetEnumerator()
+        {
+            foreach (var item in items)
+            {
+                yield return null;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
