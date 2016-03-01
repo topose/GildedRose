d8b502458e5ce30a96af0fac475b53635f71f936
topose <david.carballo.gm@gmail.com>
2016-03-01 18:15:15 +0100

    declare all utility-methods protected to be able to use them in subclasses

--- csharp/UpdatableItem.cs
+++ csharp/UpdatableItem.cs
@@ -63,37 +63,37 @@ namespace GildedRose
             return item.Name == "Backstage passes to a TAFKAL80ETC concert";
         }
 
-        private static void IncreaseQuality(Item item)
+        protected static void IncreaseQuality(Item item)
         {
             item.Quality = Min(50, item.Quality + 1);
         }
 
-        private static void DecreaseQuality(Item item)
+        protected static void DecreaseQuality(Item item)
         {
             item.Quality = Max(0, item.Quality - 1);
         }
 
-        private static void VanishQuality(Item item)
+        protected static void VanishQuality(Item item)
         {
             item.Quality = 0;
         }
 
-        private static void AgeItem(Item item)
+        protected static void AgeItem(Item item)
         {
             item.SellIn--;
         }
 
-        private static bool OutOfDate(Item item)
+        protected static bool OutOfDate(Item item)
         {
             return item.SellIn < 0;
         }
 
-        private static bool CloseToConcert(Item item)
+        protected static bool CloseToConcert(Item item)
         {
             return item.SellIn < 11;
         }
 
-        private static bool EvenCloserToMethod(Item item)
+        protected static bool EvenCloserToMethod(Item item)
         {
             return item.SellIn < 6;
         }
