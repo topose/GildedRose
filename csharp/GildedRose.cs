4652a90e5e7f718845c970c58f3440d760e74d9d
topose <david.carballo.gm@gmail.com>
2016-03-01 18:08:56 +0100

    moved UpdateQuality implementation from GildedRose to UpdatableItem, use the factory method to get the UpdatableItem and then call its UpdateQuality method

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using static System.Math;
 
 namespace GildedRose
 {
@@ -32,39 +31,7 @@ namespace GildedRose
 
         private void UpdateQuality(Item item)
         {
-            if (IsSulfuras(item)) return;
-
-            if (IsAgedBrie(item))
-            {
-                IncreaseQuality(item);
-                AgeItem(item);
-                if (OutOfDate(item))
-                    IncreaseQuality(item);
-                return;
-            }
-
-            if (IsBackStagePass(item))
-            {
-                IncreaseQuality(item);
-
-                if (CloseToConcert(item))
-                    IncreaseQuality(item);
-    
-                if (EvenCloserToMethod(item))
-                    IncreaseQuality(item);
-
-                AgeItem(item);
-
-                if (OutOfDate(item))
-                    VanishQuality(item);
-
-                return;
-            }
-
-            DecreaseQuality(item);
-            AgeItem(item);
-            if (OutOfDate(item))
-                DecreaseQuality(item);
+            GetUpdatableItem(item).UpdateQuality();   
         }
 
         private static bool IsSulfuras(Item item)
@@ -80,41 +47,6 @@ namespace GildedRose
         private static bool IsBackStagePass(Item item)
         {
             return item.Name == "Backstage passes to a TAFKAL80ETC concert";
-        }
-
-        private static void IncreaseQuality(Item item)
-        {
-            item.Quality = Min(50, item.Quality + 1);
-        }
-
-        private static void DecreaseQuality(Item item)
-        {
-            item.Quality = Max(0, item.Quality - 1);
-        }
-        
-        private static void VanishQuality(Item item)
-        {
-            item.Quality = 0;
-        }
-
-        private static void AgeItem(Item item)
-        {
-            item.SellIn--;
-        }
-
-        private static bool OutOfDate(Item item)
-        {
-            return item.SellIn < 0;
-        }
-
-        private static bool CloseToConcert(Item item)
-        {
-            return item.SellIn < 11;
-        }
-
-        private static bool EvenCloserToMethod(Item item)
-        {
-            return item.SellIn < 6;
         }
     }
 
--- csharp/UpdatableItem.cs
+++ csharp/UpdatableItem.cs
@@ -1,4 +1,6 @@
-﻿namespace GildedRose
+﻿using static System.Math;
+
+namespace GildedRose
 {
     class UpdatableItem : IUpdatableItem
     {
@@ -8,11 +10,92 @@
         {
             this.item = item;
         }
-
-
+        
         public void UpdateQuality()
         {
-            throw new System.NotImplementedException();
+            if (IsSulfuras(item)) return;
+
+            if (IsAgedBrie(item))
+            {
+                IncreaseQuality(item);
+                AgeItem(item);
+                if (OutOfDate(item))
+                    IncreaseQuality(item);
+                return;
+            }
+
+            if (IsBackStagePass(item))
+            {
+                IncreaseQuality(item);
+
+                if (CloseToConcert(item))
+                    IncreaseQuality(item);
+
+                if (EvenCloserToMethod(item))
+                    IncreaseQuality(item);
+
+                AgeItem(item);
+
+                if (OutOfDate(item))
+                    VanishQuality(item);
+
+                return;
+            }
+
+            DecreaseQuality(item);
+            AgeItem(item);
+            if (OutOfDate(item))
+                DecreaseQuality(item);
+        }
+
+        private static bool IsSulfuras(Item item)
+        {
+            return item.Name == "Sulfuras, Hand of Ragnaros";
+        }
+
+        private static bool IsAgedBrie(Item item)
+        {
+            return item.Name == "Aged Brie";
+        }
+
+        private static bool IsBackStagePass(Item item)
+        {
+            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        private static void IncreaseQuality(Item item)
+        {
+            item.Quality = Min(50, item.Quality + 1);
+        }
+
+        private static void DecreaseQuality(Item item)
+        {
+            item.Quality = Max(0, item.Quality - 1);
+        }
+
+        private static void VanishQuality(Item item)
+        {
+            item.Quality = 0;
+        }
+
+        private static void AgeItem(Item item)
+        {
+            item.SellIn--;
+        }
+
+        private static bool OutOfDate(Item item)
+        {
+            return item.SellIn < 0;
+        }
+
+        private static bool CloseToConcert(Item item)
+        {
+            return item.SellIn < 11;
+        }
+
+        private static bool EvenCloserToMethod(Item item)
+        {
+            return item.SellIn < 6;
         }
     }
 }
