3e0eeeaa7d41cd25b5cce2a8bb957c8839d0dc16
topose <david.carballo.gm@gmail.com>
2016-03-04 13:54:13 +0100

    extract updatable item factory method to class

---
+++ csharp/UpdatableItemFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace GildedRose
+{
+    class UpdatableItemFactory
+    {
+        public static IUpdatableItem CreateUpdatableItem(Item item)
+        {
+            if (IsLegendaryItem(item)) return new LegendaryItem(item);
+
+            if (IsAgedBrie(item)) return new AgedBrieItem(item);
+
+            if (IsBackStagePass(item)) return new BackStagePassItem(item);
+
+            if (IsConjured(item)) return new ConjuredItem(item);
+
+            return new NormalItem(item);
+        }
+
+        private static bool IsLegendaryItem(Item item) => item.Name == "Sulfuras, Hand of Ragnaros";
+
+        private static bool IsAgedBrie(Item item) => item.Name == "Aged Brie";
+
+        private static bool IsBackStagePass(Item item) => item.Name == "Backstage passes to a TAFKAL80ETC concert";
+
+        private static bool IsConjured(Item item) => item.Name == "Conjured";
+    }
+}
--- csharp/UpdatableItems.cs
+++ csharp/UpdatableItems.cs
@@ -17,31 +17,10 @@ namespace GildedRose
         {
             foreach (var item in items)
             {
-                yield return CreateUpdatableItem(item);
+                yield return UpdatableItemFactory.CreateUpdatableItem(item);
             }
         }
 
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
-
-        private IUpdatableItem CreateUpdatableItem(Item item)
-        {
-            if (IsLegendaryItem(item)) return new LegendaryItem(item);
-
-            if (IsAgedBrie(item)) return new AgedBrieItem(item);
-
-            if (IsBackStagePass(item)) return new BackStagePassItem(item);
-
-            if (IsConjured(item)) return new ConjuredItem(item);
-
-            return new NormalItem(item);
-        }
-
-        private static bool IsLegendaryItem(Item item) => item.Name == "Sulfuras, Hand of Ragnaros";
-
-        private static bool IsAgedBrie(Item item) => item.Name == "Aged Brie";
-
-        private static bool IsBackStagePass(Item item) => item.Name == "Backstage passes to a TAFKAL80ETC concert";
-
-        private static bool IsConjured(Item item) => item.Name == "Conjured";
     }
 }
